3602dbb530598679adc56b0a2b7e35ffde2ad64c
Sebastian Blumenthal <blumenthal@locomotec.com>
2009-09-24 16:07:05 +0000

    The build folder for the 6DSLAM library is now automaticaly created

--- install.sh
+++ install.sh
@@ -3,20 +3,27 @@
 # date: 24.09.2009
 
 echo "Starting to install BRICS_3D library."
-if [ ! -d ./build ]
-then 
-   mkdir ./build
-fi
+
 
 echo ""
 echo "Compilation of 6DSLAM precondition:"
+if [ ! -d ./external/6dslam/build ]
+then 
+   mkdir ./external/6dslam/build 
+fi
 cd ./external/6dslam/build/
 cmake ..
 make
 cd ../../../
 
+
 echo ""
 echo "Compilation of BRICS_3D library:"
+if [ ! -d ./build ]
+then 
+   mkdir ./build
+fi
+
 cd build #compile as out-of-source build
 cmake ..
 make
