30237a0977261f4a0dec96828528eee6a391e4b4
Sebastian Blumenthal <blumenthal@locomotec.com>
2009-12-30 11:31:54 +0000

    Updates for colored point cloud. It might make sense to investigate if a PointCloud is implemented as a template.

--- src/core/ColoredPointCloud3D.cpp
+++ src/core/ColoredPointCloud3D.cpp
@@ -7,34 +7,48 @@
  */
 
 #include "ColoredPointCloud3D.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
 
 using std::vector;
+using std::string;
+using std::runtime_error;
+using std::stringstream;
+using std::endl;
 
 namespace BRICS_3D {
 
 ColoredPointCloud3D::ColoredPointCloud3D() {
-	pointCloud = new vector<ColoredPoint3D>();
-	pointCloud->clear();
+	this->pointCloud = new vector<ColoredPoint3D>();
+	this->pointCloud->clear();
 
 }
 
 ColoredPointCloud3D::~ColoredPointCloud3D() {
-	if (pointCloud != NULL) {
-		pointCloud->clear();
-		delete pointCloud;
+	if (this->pointCloud != NULL) {
+		this->pointCloud->clear();
+		delete this->pointCloud;
 	}
 }
 
 unsigned int ColoredPointCloud3D::getSize() {
-	return pointCloud->size();
+	return this->pointCloud->size();
 }
 
 void ColoredPointCloud3D::addPoint(ColoredPoint3D point) {
-	pointCloud->push_back(point);
+	this->pointCloud->push_back(point);
 }
 
+//std::vector<Point3D>* ColoredPointCloud3D::getPointCloud() {
+//	return dynamic_cast<std::vector<Point3D>>(this->pointCloud);
+//}
+
 std::vector<ColoredPoint3D>* ColoredPointCloud3D::getPointCloud() {
-	return pointCloud;
+//std::vector<ColoredPoint3D>* ColoredPointCloud3D::getColoredPointCloud() {
+	return this->pointCloud;
 
 }
 
@@ -42,6 +56,36 @@ void ColoredPointCloud3D::setPointCloud(std::vector<ColoredPoint3D>* pointCloud)
 	this->pointCloud = pointCloud;
 }
 
+istream& operator>>(istream &inStream, ColoredPointCloud3D &pointCloud) {
+	ColoredPoint3D *tmpPoint;
+	string line;
+
+	while(getline(inStream, line)) {
+		tmpPoint = new ColoredPoint3D;
+		stringstream lineStream(line);
+		if (!lineStream.good()) {
+			throw runtime_error("ERROR: cannot read point.");
+		}
+		lineStream >> *tmpPoint;
+		pointCloud.addPoint(tmpPoint);
+	}
+
+	return inStream;
+}
+
+ostream& operator<<(ostream &outStream, ColoredPointCloud3D &pointCloud) {
+	for (unsigned int i = 0; i < pointCloud.getSize(); ++i) {
+		outStream << (*pointCloud.getPointCloud())[i] << endl;
+	}
+
+	return outStream;
+}
+
+void ColoredPointCloud3D::homogeneousTransformation(IHomogeneousMatrix44 *transformation) {
+	for (unsigned int i = 0; i < this->pointCloud->size(); ++i) {
+		(*this->pointCloud)[i].homogeneousTransformation(transformation);
+	}
+}
 
 }
 
--- src/core/ColoredPointCloud3D.h
+++ src/core/ColoredPointCloud3D.h
@@ -18,6 +18,16 @@ namespace BRICS_3D {
  * @brief Class to represent a Cartesian 3D point cloud with color information
  *
  */
+/* Currently (nearly) all the functionality of PointCloud3D is reimlemented, although ColoredPointCloud3D is a derivative of PointCloud3D!
+ * This is necessary, as the internal data structure is changed from std::vector<Point3D>* pointCloud  to std::vector<ColoredPoint3D>* pointCloud.
+ * The vector itself is not polymorph as it is a template. Therefore the std::vector<ColoredPoint3D>* pointCloud cannot be up-casted to std::vector<Point3D>* pointCloud.
+ * Whenever a non-overridden, inherited function in ColoredPointCloud is invoked the method works with an empty vector from the base class...
+ *
+ * To prevent too much code duplication the refactorization to a template might be a better solution - but when the point cloud is a template then dynamic polymorphism can not be applied anymore...
+ * forcing algorithms to have an overloaded function with all possible template variants - certainly something we do not want...
+ *
+ *
+ */
 class ColoredPointCloud3D : public PointCloud3D {
 public:
 
@@ -43,17 +53,49 @@ public:
 	 */
 	void addPoint(ColoredPoint3D point);
 
+//	std::vector<Point3D>* getPointCloud();
+
 	/**
 	 * @brief Get the reference to the colored point cloud
 	 * @return Pointer to point cloud
 	 */
 	std::vector<ColoredPoint3D>* getPointCloud();
+//	std::vector<ColoredPoint3D>* getColoredPointCloud();
 
 	/**
 	 * @brief Set the reference to the colored point cloud
 	 * @param pointCloud Pointer to new colored point cloud
 	 */
 	void setPointCloud(std::vector<ColoredPoint3D>* pointCloud);
+
+	/**
+	 * @brief Overridden >> operator.
+	 *
+	 * Reads a point cloud from a stream an stores it. E.g. e.g. std::cin >> coloredPointCloudObj;
+	 *
+	 * @param inStream The input stream
+	 * @param pointCloud Pointer to point where input is stored
+	 * @return Input stream
+	 */
+	friend istream& operator>>(istream &inStream, ColoredPointCloud3D &pointCloud);
+
+	/**
+	 * @brief Overridden << operator.
+	 *
+	 * Writes a point cloud to a stream e.g. std::cout << coloredPointCloudObj;
+	 *
+	 * @param outStream The output stream
+	 * @param pointCloud Pointer to point which data is forwarded to output stream
+	 * @return Output stream
+	 */
+	friend ostream& operator<<(ostream &outStream, ColoredPointCloud3D &pointCloud);
+
+	/**
+	 * @brief Applies a homogeneous transformation to the point cloud
+	 *
+	 * @param[in] transformation The homogeneous transformation matrix that will be applied
+	 */
+	void homogeneousTransformation(IHomogeneousMatrix44 *transformation);
 
 protected:
 
