c1a531931b008900fd5cab032c24e8ca39f3df51
Pinaki Banerjee <pinaki2999@gmail.com>
2011-10-28 14:23:33 +0000

    Adde extract_object_test class

--- examples/extract_object_clusters.cpp
+++ examples/extract_object_clusters.cpp
@@ -38,66 +38,75 @@ private:
 	BRICS_3D::ColorSpaceConvertor colorSpaceConvertor;
 	BRICS_3D::Centroid3D centroid3DEstimator;
 	Eigen::Vector3d centroid;
+	bool processingDone;
 public:
 
 	BRICS_3D::OSGPointCloudVisualizer viewer;
 
+	void processData(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud){
+		if(!processingDone){
+			processingDone = true;
+
+
+			// The magic starts here:P
+			cout << "[CHEAT] Recieved Kinect Cloud Size " << cloud->size() << endl;
+
+			//Converting from PCL to BRICS_3D datatype
+			BRICS_3D::ColoredPointCloud3D *inCloud = new BRICS_3D::ColoredPointCloud3D();
+			BRICS_3D::PointCloud3D *inCloudVis = new BRICS_3D::ColoredPointCloud3D();
+			pclTypecaster.convertToBRICS3DDataType(cloud,inCloudVis);
+
+			clock_t startTime = clock();
+			pclTypecaster.convertToBRICS3DDataType(cloud,inCloud);
+//			cout << "[CHEAT] Converted Input Cloud Size " << inCloud->getSize() << "\tExectution Time "
+//					<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
+
+			//Extracting the HSV based Region of Interest
+			BRICS_3D::PointCloud3D *extractedRoiHSV = new BRICS_3D::PointCloud3D();
+			startTime = clock();
+			roiExtractor.extractColorBasedROI(inCloud,extractedRoiHSV);
+//			cout << "[CHEAT] ROI Extracted Cloud Size " << extractedRoiHSV->getSize() << "\t\tExectution Time "
+//					<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
+			//Extracting the Object Clusters
+			vector<BRICS_3D::PointCloud3D*> pclExtractedClusters;
+			startTime = clock();
+			pclClusterExtractor.extractClusters(extractedRoiHSV,&pclExtractedClusters);
+//			cout << "[CHEAT] No of clusters extracted (PCL): " << pclExtractedClusters.size() << "\t\tExectution Time "
+//					<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
+
+
+			//Extracting the Object Clusters
+			//		vector<BRICS_3D::PointCloud3D*> extractedClusters;
+			//		startTime = clock();
+			//		clusterExtractor.extractClusters(extractedRoiHSV,&extractedClusters);
+			//		cout << "[CHEAT] No of clusters extracted : " << extractedClusters.size() << "\t\tExectution Time "
+			//				<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
+
+			//Obtaining the inial 3D pose
+			for (size_t i=0; i< pclExtractedClusters.size(); i++){
+				centroid = centroid3DEstimator.computeCentroid(pclExtractedClusters[i]);
+//				cout << "[CHEAT] Pose of Object-" << i+1 << " is : ["<<centroid[0] << ", "
+//						<< centroid[1] << ", " << centroid[2] << "]" << endl;
+			}
+
+			//Visualizing the output
+
+//			viewer.addPointCloud(inCloudVis);
+			viewer.addPointCloud(extractedRoiHSV, 1, 0, 0, 0);
+			viewer.clearButLast();
+
+
+			delete inCloud;
+			delete inCloudVis;
+//			delete extractedRoiHSV;
+//			pclExtractedClusters.clear();
+
+			processingDone = false;
+		}
+	}
 
 	void callback (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud){
-
-// The magic starts here:P
-		cout << "[CHEAT] Recieved Kinect Cloud Size " << cloud->size() << endl;
-
-		//Converting from PCL to BRICS_3D datatype
-		BRICS_3D::ColoredPointCloud3D *inCloud = new BRICS_3D::ColoredPointCloud3D();
-		BRICS_3D::PointCloud3D *inCloudVis = new BRICS_3D::ColoredPointCloud3D();
-		pclTypecaster.convertToBRICS3DDataType(cloud,inCloudVis);
-
-		clock_t startTime = clock();
-		pclTypecaster.convertToBRICS3DDataType(cloud,inCloud);
-		cout << "[CHEAT] Converted Input Cloud Size " << inCloud->getSize() << "\tExectution Time "
-				<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
-
-		//Extracting the HSV based Region of Interest
-		BRICS_3D::PointCloud3D *extractedRoiHSV = new BRICS_3D::PointCloud3D();
-		startTime = clock();
-		roiExtractor.extractColorBasedROI(inCloud,extractedRoiHSV);
-		cout << "[CHEAT] ROI Extracted Cloud Size " << extractedRoiHSV->getSize() << "\t\tExectution Time "
-				<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
-
-		//Extracting the Object Clusters
-		vector<BRICS_3D::PointCloud3D*> pclExtractedClusters;
-		startTime = clock();
-		pclClusterExtractor.extractClusters(extractedRoiHSV,&pclExtractedClusters);
-		cout << "[CHEAT] No of clusters extracted (PCL): " << pclExtractedClusters.size() << "\t\tExectution Time "
-				<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
-
-
-		//Extracting the Object Clusters
-//		vector<BRICS_3D::PointCloud3D*> extractedClusters;
-//		startTime = clock();
-//		clusterExtractor.extractClusters(extractedRoiHSV,&extractedClusters);
-//		cout << "[CHEAT] No of clusters extracted : " << extractedClusters.size() << "\t\tExectution Time "
-//				<< double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." <<endl;
-
-		//Obtaining the inial 3D pose
-		for (size_t i=0; i< pclExtractedClusters.size(); i++){
-			centroid = centroid3DEstimator.computeCentroid(pclExtractedClusters[i]);
-			cout << "[CHEAT] Pose of Object-" << i+1 << " is : ["<<centroid[0] << ", "
-					<< centroid[1] << ", " << centroid[2] << "]" << endl;
-		}
-
-		//Visualizing the output
-		viewer.clearButLast();
-		viewer.addPointCloud(inCloudVis);
-		viewer.addPointCloud(extractedRoiHSV, 1, 0, 0, 0);
-
-
-
-		delete inCloud;
-		delete extractedRoiHSV;
-		pclExtractedClusters.clear();
-
+		processData(cloud);
 	}
 
 
@@ -115,17 +124,19 @@ public:
 		clusterExtractor.setMaxClusterSize(10000);
 		clusterExtractor.setClusterTolerance(0.02);
 
-				pclClusterExtractor.setMinClusterSize(100);
-				pclClusterExtractor.setMaxClusterSize(10000);
-				pclClusterExtractor.setClusterTolerance(0.02);
+		pclClusterExtractor.setMinClusterSize(100);
+		pclClusterExtractor.setMaxClusterSize(10000);
+		pclClusterExtractor.setClusterTolerance(0.02);
 
 
 		interface = new pcl::OpenNIGrabber();
-	    boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f = boost::bind (&ExtractObjectClusters::callback, this, _1);
-	    interface->registerCallback (f);
-	    interface->start ();
+		boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f = boost::bind (&ExtractObjectClusters::callback, this, _1);
+		interface->registerCallback (f);
+		interface->start ();
 
-	    cout << "[CHEAT] Initialization Done" << endl;
+		processingDone=false;
+
+		cout << "[CHEAT] Initialization Done" << endl;
 	}
 
 };
@@ -135,10 +146,10 @@ int main(){
 	ExtractObjectClusters objectClusterExtractor;
 	objectClusterExtractor.keepLooking();
 
-    while (!objectClusterExtractor.viewer.done())
-    {
-      sleep (1);
-    }
+	while (!objectClusterExtractor.viewer.done())
+	{
+		sleep (1);
+	}
 
 
 }
