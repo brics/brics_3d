1a6fac2e2c5960d8760c34b79e12a3ac41dbe725
Pinaki Banerjee <pinaki2999@gmail.com>
2011-12-01 16:36:38 +0000

    updated circular wrapping for min-max limit of hs values

--- src/algorithm/filtering/ColorBasedROIExtractorHSV.cpp
+++ src/algorithm/filtering/ColorBasedROIExtractorHSV.cpp
@@ -1,21 +1,21 @@
 /******************************************************************************
-* BRICS_3D - 3D Perception and Modeling Library
-* Copyright (c) 2011, GPS GmbH
-*
-* Author: Pinaki Sunil Banerjee
-*
-*
-* This software is published under a dual-license: GNU Lesser General Public
-* License LGPL 2.1 and Modified BSD license. The dual-license implies that
-* users of this code may choose which terms they prefer.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License LGPL and the BSD license for
-* more details.
-*
-******************************************************************************/
+ * BRICS_3D - 3D Perception and Modeling Library
+ * Copyright (c) 2011, GPS GmbH
+ *
+ * Author: Pinaki Sunil Banerjee
+ *
+ *
+ * This software is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and Modified BSD license. The dual-license implies that
+ * users of this code may choose which terms they prefer.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for
+ * more details.
+ *
+ ******************************************************************************/
 
 #include "ColorBasedROIExtractorHSV.h"
 
@@ -115,15 +115,13 @@ void ColorBasedROIExtractorHSV::extractColorBasedROI(BRICS_3D::ColoredPointCloud
 	BRICS_3D::Point3D tempPoint3D;
 	out_cloud->getPointCloud()->clear();
 
-//	printf("Used H-S Limits for extraction: H:[%f %f] S:[%f %f]\n", minH, maxH, minS, maxS);
-
+		printf("Used H-S Limits for extraction: H:[%f %f] S:[%f %f]\n", minH, maxH, minS, maxS);
 	for (unsigned int i = 0; i < static_cast<unsigned int>(cloudSize); i++) {
-
 		passed = false;
 		//Getting the HSV values for the RGB points
 		tempChar = in_cloud->getPointCloud()->data()[i].red;
-				tempR = tempChar << 0;
-				tempR = abs(tempR);
+		tempR = tempChar << 0;
+		tempR = abs(tempR);
 
 		tempChar = in_cloud->getPointCloud()->data()[i].green;
 		tempG = tempChar << 0;
@@ -134,35 +132,81 @@ void ColorBasedROIExtractorHSV::extractColorBasedROI(BRICS_3D::ColoredPointCloud
 		tempB = abs(tempB);
 
 		colorConvertor.rgbToHsv(tempR, tempG, tempB, &tempH, &tempS, &tempV);
+		if(minH < maxH){
 
-		//Checking the values with the set limits
-		if (tempH <= maxH && tempH >= minH) {
-			if (tempS >= minS && tempS <= maxS) {
-				passed=true;
 
+			//Checking the values with the set limits
+			if (tempH <= maxH && tempH >= minH) {
+				if (tempS >= minS && tempS <= maxS) {
+					passed=true;
+
+				}
+			}
+
+
+			//		printf("H-S Limits: [%f %f %f %f]\n", minH, maxH, minS, maxS);
+			//		printf("Actual H-S Values: [%d %d %d %f %f]\n", tempR, tempG, tempB, tempH, tempS);
+			if(passed){
+				BRICS_3D::Point3D *tempPoint3D =  new BRICS_3D::Point3D(in_cloud->getPointCloud()->data()[i].getX(),
+						in_cloud->getPointCloud()->data()[i].getY(),
+						in_cloud->getPointCloud()->data()[i].getZ());
+
+				BRICS_3D::ColoredPoint3D *tempColoredPoint3D = new BRICS_3D::ColoredPoint3D(tempPoint3D,
+						in_cloud->getPointCloud()->data()[i].red,
+						in_cloud->getPointCloud()->data()[i].green,
+						in_cloud->getPointCloud()->data()[i].blue);
+
+				out_cloud->addPoint(tempColoredPoint3D);
+				delete tempPoint3D;
+				delete tempColoredPoint3D;
+			}
+
+
+		}else {
+			//		passed = false;
+			//		//Getting the HSV values for the RGB points
+			//		tempChar = in_cloud->getPointCloud()->data()[i].red;
+			//		tempR = tempChar << 0;
+			//		tempR = abs(tempR);
+			//
+			//		tempChar = in_cloud->getPointCloud()->data()[i].green;
+			//		tempG = tempChar << 0;
+			//		tempG = abs(tempG);
+			//
+			//		tempChar = in_cloud->getPointCloud()->data()[i].blue;
+			//		tempB = tempChar << 0;
+			//		tempB = abs(tempB);
+			//
+			//		colorConvertor.rgbToHsv(tempR, tempG, tempB, &tempH, &tempS, &tempV);
+
+			//Checking the values with the set limits
+			if ((tempH <= 255 && tempH >= minH) || (tempH >= 0 && tempH <= maxH)) {
+				if (tempS >= minS && tempS <= maxS) {
+					passed=true;
+
+				}
+			}
+
+
+			//		printf("H-S Limits: [%f %f %f %f]\n", minH, maxH, minS, maxS);
+			//		printf("Actual H-S Values: [%d %d %d %f %f]\n", tempR, tempG, tempB, tempH, tempS);
+			if(passed){
+				BRICS_3D::Point3D *tempPoint3D =  new BRICS_3D::Point3D(in_cloud->getPointCloud()->data()[i].getX(),
+						in_cloud->getPointCloud()->data()[i].getY(),
+						in_cloud->getPointCloud()->data()[i].getZ());
+
+				BRICS_3D::ColoredPoint3D *tempColoredPoint3D = new BRICS_3D::ColoredPoint3D(tempPoint3D,
+						in_cloud->getPointCloud()->data()[i].red,
+						in_cloud->getPointCloud()->data()[i].green,
+						in_cloud->getPointCloud()->data()[i].blue);
+
+				out_cloud->addPoint(tempColoredPoint3D);
+				delete tempPoint3D;
+				delete tempColoredPoint3D;
 			}
 		}
-
-//		printf("H-S Limits: [%f %f %f %f]\n", minH, maxH, minS, maxS);
-//		printf("Actual H-S Values: [%d %d %d %f %f]\n", tempR, tempG, tempB, tempH, tempS);
-		if(passed){
-			BRICS_3D::Point3D *tempPoint3D =  new BRICS_3D::Point3D(in_cloud->getPointCloud()->data()[i].getX(),
-										in_cloud->getPointCloud()->data()[i].getY(),
-										in_cloud->getPointCloud()->data()[i].getZ());
-
-			BRICS_3D::ColoredPoint3D *tempColoredPoint3D = new BRICS_3D::ColoredPoint3D(tempPoint3D,
-					in_cloud->getPointCloud()->data()[i].red,
-					in_cloud->getPointCloud()->data()[i].green,
-					in_cloud->getPointCloud()->data()[i].blue);
-
-			out_cloud->addPoint(tempColoredPoint3D);
-			delete tempPoint3D;
-			delete tempColoredPoint3D;
-		}
-
-
 	}
-//	printf("Output cloud size:%d\n", out_cloud->getSize());
+	//	printf("Output cloud size:%d\n", out_cloud->getSize());
 }
 
 
@@ -184,15 +228,15 @@ void ColorBasedROIExtractorHSV::extractColorBasedROI(BRICS_3D::ColoredPointCloud
 	BRICS_3D::Point3D tempPoint3D;
 	out_cloud->getPointCloud()->clear();
 
-//	printf("Used H-S Limits for extraction: H:[%f %f] S:[%f %f]\n", minH, maxH, minS, maxS);
+		printf("Used H-S Limits for extraction: H:[%f %f] S:[%f %f]\n", minH, maxH, minS, maxS);
 
 	for (unsigned int i = 0; i < static_cast<unsigned int>(cloudSize); i++) {
 
 		passed = false;
 		//Getting the HSV values for the RGB points
 		tempChar = in_cloud->getPointCloud()->data()[i].red;
-				tempR = tempChar << 0;
-				tempR = abs(tempR);
+		tempR = tempChar << 0;
+		tempR = abs(tempR);
 
 		tempChar = in_cloud->getPointCloud()->data()[i].green;
 		tempG = tempChar << 0;
@@ -212,13 +256,13 @@ void ColorBasedROIExtractorHSV::extractColorBasedROI(BRICS_3D::ColoredPointCloud
 			}
 		}
 
-//		printf("H-S Limits: [%f %f %f %f]\n", minH, maxH, minS, maxS);
-//		printf("Actual H-S Values: [%d %d %d %f %f]\n", tempR, tempG, tempB, tempH, tempS);
+		//		printf("H-S Limits: [%f %f %f %f]\n", minH, maxH, minS, maxS);
+		//		printf("Actual H-S Values: [%d %d %d %f %f]\n", tempR, tempG, tempB, tempH, tempS);
 
 		if(passed){
 			BRICS_3D::Point3D *tempPoint3D =  new BRICS_3D::Point3D(in_cloud->getPointCloud()->data()[i].getX(),
-										in_cloud->getPointCloud()->data()[i].getY(),
-										in_cloud->getPointCloud()->data()[i].getZ());
+					in_cloud->getPointCloud()->data()[i].getY(),
+					in_cloud->getPointCloud()->data()[i].getZ());
 
 			out_cloud->addPoint(tempPoint3D);
 			delete tempPoint3D;
@@ -226,7 +270,7 @@ void ColorBasedROIExtractorHSV::extractColorBasedROI(BRICS_3D::ColoredPointCloud
 		}
 
 	}
-//	printf("Output cloud size:%d\n", out_cloud->getSize());
+	//	printf("Output cloud size:%d\n", out_cloud->getSize());
 }
 
 
