950bec8eeccb81db4407ecc9cbc0a2ca1306fbf1
Sebastian Blumenthal <blumenthal@locomotec.com>
2014-04-22 12:51:53 +0200

    HDF5 example extended.

--- examples/wm_hdf5_test.cpp
+++ examples/wm_hdf5_test.cpp
@@ -17,10 +17,30 @@
 *
 ******************************************************************************/
 
-/* BRICS_3D includes */
+/*
+ * This example illustrates a distributed world model with two world model
+ * instances involved (aka delegates).
+ *
+ * The second instance is an exact "replica" of the first one.
+ * In this case all updates on the world model will be forwarded to the replica
+ * as well. A single update involves "serialization" via HDF5, transmission of
+ * data (byte stream) and deserialization on the reciever's side.
+ *
+ *  +----------+                +-------------+
+ *  |    wm    |  ---------->   |  wmReplica  |
+ *  +----------+    updates     +-------------+
+ *
+ * As an example the world model instance could be deployed on a tablet PC to
+ * control a robot. The world model replica is then used by a particular robot
+ * to influence its internal base motion controller.
+ *
+ * We assume the motion controller needs spheres as obstacles and a bounding box
+ * a virtual fence.
+ *
+ */
 
+/* BRICS_3D includes */
 #include <brics_3d/worldModel/sceneGraph/OSGVisualizer.h>
-#include <brics_3d/core/PointCloud3D.h>
 #include <brics_3d/core/Logger.h>
 #include <brics_3d/core/HomogeneousMatrix44.h>
 #include <brics_3d/worldModel/WorldModel.h>
@@ -34,6 +54,7 @@
 using namespace brics_3d;
 using brics_3d::Logger;
 
+// Just a helper tool to print an update message
 void hexdump(const char *ptr, int buflen) {
 	unsigned char *buf = (unsigned char*)ptr;
 	int i, j;
@@ -52,14 +73,22 @@ void hexdump(const char *ptr, int buflen) {
 	}
 }
 
-// "Is" an output port and "has" an input port to directly feed forward data (byte array).
+/*
+ * Implementation of data transmission.
+ *
+ * In a real application this should be replaced by an implementation for
+ * a certain communication framework (e.g. ROS, OROCOS, LooCI, sockets, USB, etc.).
+ *
+ * The HSDF5SimleBridge "is" an output port and "has" an input port to directly
+ * feed forward data (byte array).
+ */
 class HSDF5SimleBridge : public brics_3d::rsg::IOutputPort {
 public:
 	HSDF5SimleBridge(brics_3d::rsg::IInputPort* inputPort) : inputPort(inputPort){};
 	virtual ~HSDF5SimleBridge(){};
 
 	int write(const char *dataBuffer, int dataLength, int &transferredBytes) {
-		LOG(INFO) << "HSDF5SimleBridge: Feeding data forwards.";
+		LOG(DEBUG) << "HSDF5SimleBridge: Feeding data forwards.";
 //		hexdump(dataBuffer, dataLength);
 		return inputPort->write(dataBuffer, dataLength, transferredBytes); // just feed forward
 	};
@@ -68,31 +97,16 @@ private:
 	brics_3d::rsg::IInputPort* inputPort;
 };
 
-class OutputPortDummy : public brics_3d::rsg::IOutputPort {
-public:
-	OutputPortDummy(){};
-	virtual ~OutputPortDummy(){};
-	int write(const char *dataBuffer, int dataLength, int &transferredBytes){
-		hexdump(dataBuffer, dataLength);
-		transferredBytes = dataLength;
-		return 0;
-	};
-};
-
-
 int main(int argc, char **argv) {
 
-	std::string dataSetFolder = BRICS_MODELS_DIR;
-	std::string dataSet = dataSetFolder + "/bunny000.txt";
-
-	/* Configure logger - default level won't tell us much */
+	/* Configure the logger - default level won't tell us much */
 	brics_3d::Logger::setMinLoglevel(brics_3d::Logger::LOGDEBUG);
 
-	/* Create WM  handles */
-	brics_3d::WorldModel* wm = new brics_3d::WorldModel();
-	brics_3d::WorldModel* wmReplica = new brics_3d::WorldModel();
+	/* Create the world model instances/handles */
+	brics_3d::WorldModel* wm = new brics_3d::WorldModel();        // first delegate
+	brics_3d::WorldModel* wmReplica = new brics_3d::WorldModel(); // second delegate
 
-	/* Attach some visualization facilities */
+	/* Attach some (optional) visualization facilities (observers) */
 	brics_3d::rsg::OSGVisualizer* wm3DVisualizer = new brics_3d::rsg::OSGVisualizer();
 	brics_3d::rsg::DotVisualizer* wmStructureVisualizer = new brics_3d::rsg::DotVisualizer(&wm->scene);
 	brics_3d::rsg::DotVisualizer* wmReplicaStructureVisualizer = new brics_3d::rsg::DotVisualizer(&wmReplica->scene);
@@ -112,31 +126,24 @@ int main(int argc, char **argv) {
 	usleep(500*1000); // the OSG visualization window seems to need some setup time
 
 	/*
-	 * Connect both world models via:
-	 * some update on first world model -> call observer class -> serialize
-	 * -> send byte steam via HSDF5SimleBridge -> deserialize -> update on second world model
+	 * Connect both world model instances via update mechanism as follows:
+	 * 1.) some update on first world model
+	 * 2.) call attached observer class
+	 * 3.) serialize
+	 * 4.) send byte steam via HSDF5SimleBridge
+	 * 5.) deserialize
+	 * 6.) according update on second world model
 	 */
 	brics_3d::rsg::HDF5UpdateDeserializer* wmUpdatesToHdf5deserializer = new brics_3d::rsg::HDF5UpdateDeserializer(wmReplica);
-	//OutputPortDummy port;
-	HSDF5SimleBridge feedWorwardBridge(wmUpdatesToHdf5deserializer);
-	brics_3d::rsg::HDF5UpdateSerializer* wmUpdatesToHdf5Serializer = new brics_3d::rsg::HDF5UpdateSerializer(&feedWorwardBridge);
+	HSDF5SimleBridge* feedForwardBridge = new HSDF5SimleBridge(wmUpdatesToHdf5deserializer);
+	brics_3d::rsg::HDF5UpdateSerializer* wmUpdatesToHdf5Serializer = new brics_3d::rsg::HDF5UpdateSerializer(feedForwardBridge);
 	wm->scene.attachUpdateObserver(wmUpdatesToHdf5Serializer);
 
-	/* Load a data set */
-	brics_3d::PointCloud3D::PointCloud3DPtr pointCloud1(new brics_3d::PointCloud3D());
-	brics_3d::rsg::PointCloud<brics_3d::PointCloud3D>::PointCloudPtr pointCloud1Container(new brics_3d::rsg::PointCloud<brics_3d::PointCloud3D>());
-	pointCloud1Container->data = pointCloud1;
-	pointCloud1Container->data->readFromTxtFile(dataSet);
 
-	/* Add point cloud to wm */
-    brics_3d::rsg::Id pointCloud1Id = 0;
-    std::vector<brics_3d::rsg::Attribute> attributes;
-    attributes.clear();
-    attributes.push_back(brics_3d::rsg::Attribute("name","pointCloud1Id"));
-	wm->scene.addGeometricNode(wm->getRootNodeId(), pointCloud1Id, attributes, pointCloud1Container, wm->now());
-
+	/* ================== Setup of world model content ===================== */
 
 	/* Add group nodes */
+	std::vector<brics_3d::rsg::Attribute> attributes;
 	attributes.clear();
 	attributes.push_back(rsg::Attribute("taskType", "scene_objecs"));
 	rsg::Id sceneObjectsId;
@@ -193,11 +200,122 @@ int main(int argc, char **argv) {
 	rsg::Id sphere2Id;
 	wm->scene.addGeometricNode(sphere2TfId, sphere2Id, attributes, sphere2, wm->now());
 
+	LOG(INFO) << "Done with setup of world model.";
+
+	/* ============ Sample queries by the motion controller ================ */
+
+	vector<rsg::Attribute> queryAttributes;
+	vector<rsg::Id> resultIds;
+
+	/* Get data and pose of the box */
+	queryAttributes.clear();
+	resultIds.clear();
+	queryAttributes.push_back(rsg::Attribute("name", "virtual_fence"));
+	wmReplica->scene.getNodes(queryAttributes, resultIds);
+
+	for(vector<rsg::Id>::iterator it = resultIds.begin(); it!=resultIds.end(); ++it) { // loop over results
+		rsg::TimeStamp creationTime;
+		rsg::Shape::ShapePtr shape;
+		wmReplica->scene.getGeometry(*it, shape, creationTime);
+		rsg::Box::BoxPtr resultBox = boost::dynamic_pointer_cast<rsg::Box>(shape);
+		if (resultBox != 0) {
+			LOG(INFO) << "Box (x,y,z) = " << resultBox->getSizeX() << " "
+					<< resultBox->getSizeY() << " "
+					<< resultBox->getSizeZ();
+		}
+		IHomogeneousMatrix44::IHomogeneousMatrix44Ptr boxPose;
+		wmReplica->scene.getTransformForNode(*it, wmReplica->getRootNodeId(), creationTime, boxPose);
+		LOG(INFO) << "Pose of box is = " << std::endl << *boxPose;
+	}
+
+	/* Get data of obstacles (spheres) */
+	queryAttributes.clear();
+	resultIds.clear();
+	queryAttributes.push_back(rsg::Attribute("shape", "Sphere"));
+	wmReplica->scene.getNodes(queryAttributes, resultIds);
+
+	for(vector<rsg::Id>::iterator it = resultIds.begin(); it!=resultIds.end(); ++it) { // loop over results
+		rsg::TimeStamp creationTime;
+		rsg::Shape::ShapePtr shape;
+		wmReplica->scene.getGeometry(*it, shape, creationTime);
+		rsg::Sphere::SpherePtr resultSphere = boost::dynamic_pointer_cast<rsg::Sphere>(shape);
+		if (resultSphere != 0) {
+			LOG(INFO) << "Sphere (r) = " << resultSphere->getRadius();
+		}
+		IHomogeneousMatrix44::IHomogeneousMatrix44Ptr spherePose;
+		wmReplica->scene.getTransformForNode(*it, wmReplica->getRootNodeId(), creationTime, spherePose);
+		LOG(INFO) << "Pose of sphere is = " << std::endl << *spherePose;
+	}
 
 
+	/* =============== Sample updates to the world model =================== */
 
 
-	LOG(INFO) << "Done so far.";
+	/* update the pose of the second sphere */
+	brics_3d::IHomogeneousMatrix44::IHomogeneousMatrix44Ptr transformShpere2Update(new brics_3d::HomogeneousMatrix44(1,0,0,  	// Rotation coefficients
+	                                                             0,1,0,
+	                                                             0,0,1,
+	                                                             0.9,1.55,0)); 						// Translation coefficients
+	wm->scene.setTransform(sphere2TfId, transformShpere2Update, wm->now());
+
+	/* update fence geometry be creation of a new geometry */
+	queryAttributes.clear();
+	resultIds.clear();
+	queryAttributes.push_back(rsg::Attribute("name", "virtual_fence"));
+	wm->scene.getNodes(queryAttributes, resultIds);
+	for(vector<rsg::Id>::iterator it = resultIds.begin(); it!=resultIds.end(); ++it) { // delete all node  with "name", "virtual_fence"
+		wm->scene.deleteNode(*it);
+	}
+	attributes.clear();
+	attributes.push_back(rsg::Attribute("shape", "Box"));
+	attributes.push_back(rsg::Attribute("name", "virtual_fence"));
+	rsg::Box::BoxPtr newBox(new rsg::Box(1.5,2.5,0));
+	rsg::Id newBoxId;
+	wm->scene.addGeometricNode(boxTfId, newBoxId, attributes, newBox, wm->now());
+
+
+	/* ====== Sample queries by the motion controller after update ========= */
+
+	/* Get data and pose of the box */
+	queryAttributes.clear();
+	resultIds.clear();
+	queryAttributes.push_back(rsg::Attribute("name", "virtual_fence"));
+	wmReplica->scene.getNodes(queryAttributes, resultIds);
+
+	for(vector<rsg::Id>::iterator it = resultIds.begin(); it!=resultIds.end(); ++it) { // loop over results
+		rsg::TimeStamp creationTime;
+		rsg::Shape::ShapePtr shape;
+		wmReplica->scene.getGeometry(*it, shape, creationTime);
+		rsg::Box::BoxPtr resultBox = boost::dynamic_pointer_cast<rsg::Box>(shape);
+		if (resultBox != 0) {
+			LOG(INFO) << "Box (x,y,z) = " << resultBox->getSizeX() << " "
+					<< resultBox->getSizeY() << " "
+					<< resultBox->getSizeZ();
+		}
+		IHomogeneousMatrix44::IHomogeneousMatrix44Ptr boxPose;
+		wmReplica->scene.getTransformForNode(*it, wmReplica->getRootNodeId(), creationTime, boxPose);
+		LOG(INFO) << "Pose of box is = " << std::endl << *boxPose;
+	}
+
+	/* Get data of obstacles (spheres) */
+	queryAttributes.clear();
+	resultIds.clear();
+	queryAttributes.push_back(rsg::Attribute("shape", "Sphere"));
+	wmReplica->scene.getNodes(queryAttributes, resultIds);
+
+	for(vector<rsg::Id>::iterator it = resultIds.begin(); it!=resultIds.end(); ++it) { // loop over results
+		rsg::TimeStamp creationTime;
+		rsg::Shape::ShapePtr shape;
+		wmReplica->scene.getGeometry(*it, shape, creationTime);
+		rsg::Sphere::SpherePtr resultSphere = boost::dynamic_pointer_cast<rsg::Sphere>(shape);
+		if (resultSphere != 0) {
+			LOG(INFO) << "Sphere (r) = " << resultSphere->getRadius();
+		}
+		IHomogeneousMatrix44::IHomogeneousMatrix44Ptr spherePose;
+		wmReplica->scene.getTransformForNode(*it, wmReplica->getRootNodeId(), creationTime, spherePose);
+		LOG(INFO) << "Pose of sphere is = " << std::endl << *spherePose;
+	}
+
 
 	/* Wait until user closes the GUI */
 	while(!wm3DVisualizer->done()) {
@@ -206,8 +324,11 @@ int main(int argc, char **argv) {
 
 	/* Clean up */
 	delete wmUpdatesToHdf5Serializer;
+	delete wmUpdatesToHdf5deserializer;
+	delete feedForwardBridge;
 	delete wm3DVisualizer;
 	delete wmStructureVisualizer;
+	delete wmReplicaStructureVisualizer;
 	delete wm;
 	delete wmReplica;
 }
