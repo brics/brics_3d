3e6f0c90ad1570445a1f724f04b63df70e7161f1
Sebastian Blumenthal <blumenthal@locomotec.com>
2017-06-02 18:07:40 +0200

    Added JSONGraphGenerator class.

---
+++ src/brics_3d/worldModel/sceneGraph/JSONGraphGenerator.cpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * BRICS_3D - 3D Perception and Modeling Library
+ * Copyright (c) 2017, KU Leuven
+ *
+ * Author: Sebastian Blumenthal
+ *
+ *
+ * This software is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and Modified BSD license. The dual-license implies that
+ * users of this code may choose which terms they prefer.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for
+ * more details.
+ *
+ ******************************************************************************/
+
+#include "JSONGraphGenerator.h"
+
+namespace brics_3d {
+namespace rsg {
+
+JSONGraphGenerator::JSONGraphGenerator() : INodeVisitor(custom) {
+	reset();
+}
+
+JSONGraphGenerator::~JSONGraphGenerator() {
+	reset();
+}
+
+void JSONGraphGenerator::visit(Node* node) {
+	LOG(DEBUG) << "JSONGraphGenerator: adding a Node-" << node->getId().toString();
+
+	try {
+
+		/* the actual graph primitive */
+		libvariant::Variant jsonNode;
+		jsonNode.Set("@graphtype", libvariant::Variant("Node"));
+		JSONTypecaster::addIdToJSON(node->getId(), jsonNode, "id");
+		JSONTypecaster::addAttributesToJSON(node->getAttributes(), jsonNode);
+		TimeStamp attributesTimeStamp = node->getAttributesTimeStamp();
+		JSONTypecaster::addTimeStampToJSON(attributesTimeStamp, jsonNode, "attributesTimeStamp");
+
+		/* store it for later assembly */
+		jsonLookUpTable.insert(std::make_pair(node->getId(), jsonNode));
+
+	} catch (std::exception e) {
+		LOG(ERROR) << "JSONGraphGenerator addNode: Cannot create a JSON serialization. Exception = " << std::endl << e.what();
+	}
+}
+
+void JSONGraphGenerator::visit(Group* node) {
+
+	/* recursively go down the graph structure */
+	for(unsigned i = 0; i < node->getNumberOfChildren(); ++i) {
+		// calculate hashes recursively
+		node->getChild(i)->accept(this);
+	}
+
+	/* during unwinding of the recusrion stack: build up the JSON model */
+	LOG(DEBUG) << "JSONGraphGenerator: adding a Group-" << node->getId().toString();
+	try {
+
+		/* the actual graph primitive */
+		libvariant::Variant jsonNode;
+		jsonNode.Set("@graphtype", libvariant::Variant("Group"));
+		JSONTypecaster::addIdToJSON(node->getId(), jsonNode, "id");
+		JSONTypecaster::addAttributesToJSON(node->getAttributes(), jsonNode);
+		TimeStamp attributesTimeStamp = node->getAttributesTimeStamp();
+		JSONTypecaster::addTimeStampToJSON(attributesTimeStamp, jsonNode, "attributesTimeStamp");
+
+
+		/* attach all childs */
+		libvariant::Variant childs(libvariant::VariantDefines::ListType);
+		for(unsigned i = 0; i < node->getNumberOfChildren(); ++i) { // FIXME: already traversed childs
+			libvariant::Variant child;
+			child.Set("@childtype", libvariant::Variant("Child"));
+			child.Set("child", getJSONById(node->getChild(i)->getId()));
+			childs.Append(child);
+		}
+		jsonNode.Set("childs", childs);
+
+		/* store it for later assembly */
+		jsonLookUpTable.insert(std::make_pair(node->getId(), jsonNode));
+
+	} catch (std::exception e) {
+		LOG(ERROR) << "JSONGraphGenerator addNode: Cannot create a JSON serialization. Exception = " << std::endl << e.what();
+	}
+
+}
+
+void JSONGraphGenerator::visit(Transform* node) {
+
+}
+
+void JSONGraphGenerator::visit(GeometricNode* node) {
+
+}
+
+void JSONGraphGenerator::visit(Connection* connection) {
+
+}
+
+void JSONGraphGenerator::reset() {
+	jsonLookUpTable.clear();
+	model.Clear();
+}
+
+std::string JSONGraphGenerator::getJSON() {
+	return "false";
+}
+
+libvariant::Variant& JSONGraphGenerator::getJSONModel() {
+	return model;
+}
+
+libvariant::Variant JSONGraphGenerator::getJSONById(Id id) {
+	jsonIterator = jsonLookUpTable.find(id);
+	if (jsonIterator != jsonLookUpTable.end()) {
+		return jsonIterator->second;
+	}
+
+	LOG(ERROR) << "JSONGraphGenerator: JSON look up failed for ID " << id;
+	return libvariant::Variant(libvariant::VariantDefines::NullType);
+}
+
+
+} /* namespace rsg */
+} /* namespace brics_3d */
+
+/* EOF */
---
+++ src/brics_3d/worldModel/sceneGraph/JSONGraphGenerator.h
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * BRICS_3D - 3D Perception and Modeling Library
+ * Copyright (c) 2017, KU Leuven
+ *
+ * Author: Sebastian Blumenthal
+ *
+ *
+ * This software is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and Modified BSD license. The dual-license implies that
+ * users of this code may choose which terms they prefer.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for
+ * more details.
+ *
+ ******************************************************************************/
+
+#ifndef RSG_JSONGRAPHGENERATOR_H_
+#define RSG_JSONGRAPHGENERATOR_H_
+
+#include "Node.h"
+#include "Group.h"
+#include "Connection.h"
+#include "Transform.h"
+#include "GeometricNode.h"
+#include "Connection.h"
+#include "INodeVisitor.h"
+#include "brics_3d/util/JSONTypecaster.h"
+
+#include <sstream>
+
+namespace brics_3d {
+namespace rsg {
+
+/**
+ * @brief Node visitor that generates a RSG-JSON representation.
+ * @ingroup sceneGraph
+ */
+class JSONGraphGenerator : public INodeVisitor {
+public:
+	JSONGraphGenerator();
+	virtual ~JSONGraphGenerator();
+
+	virtual void visit(Node* node);
+	virtual void visit(Group* node);
+	virtual void visit(Transform* node);
+	virtual void visit(GeometricNode* node);
+	virtual void visit(Connection* connection);
+
+	virtual void reset();
+
+	std::string getJSON();
+	libvariant::Variant& getJSONModel();
+    libvariant::Variant getJSONById(Id id); //TODO reference?
+
+protected:
+
+	/// json model of graph
+	libvariant::Variant model;
+
+    /// Table that maps IDs to already generated JSON objects - so we can assemble them later
+    std::map<Id, libvariant::Variant > jsonLookUpTable;
+
+    /// Iterator for jsonLookUpTable
+    std::map<Id, libvariant::Variant >::const_iterator jsonIterator;
+
+};
+
+} /* namespace rsg */
+} /* namespace brics_3d */
+
+#endif /* RSG_JSONGRAPHGENERATOR_H_ */
+
+/* EOF */
